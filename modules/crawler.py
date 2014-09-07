92728387281a2a907dc80f4ff784b073dc67ac82
bacon <ibarria0@gmail.com>
2014-09-07 14:56:01 -0500

    asyncio

--- modules/crawler.py
+++ modules/crawler.py
@@ -1,4 +1,7 @@
 from multiprocessing import Pool
+from classes.Documento import Documento,Base
+from sqlalchemy.orm import sessionmaker,undefer
+from sqlalchemy import create_engine
 from modules import parser,url_generator,db_worker
 from bs4 import BeautifulSoup,SoupStrainer
 import math
@@ -6,17 +9,43 @@ from random import shuffle
 import re
 import logging
 from queue import Queue
-import sys
+import sys,os
 import gc
 sys.setrecursionlimit(10000)
 import urllib3
 import threading
 from time import sleep
+import asyncio
+import aiohttp
 
 logger = logging.getLogger('crawler')
-url = '/Sicowebconsultas/buscar.aspx'
-office_ids = [202, 43, 624, 108, 1, 29, 114, 238, 234, 105, 47, 145, 22, 103, 203, 107, 109, 16015, 106, 55, 57, 204, 315, 330, 345, 600, 115, 208, 11018, 274, 360, 110, 212, 375, 521, 522, 311, 11004, 11014, 625, 623, 628, 58, 45, 611, 614, 612, 613, 615, 616, 617, 618, 610, 619, 31, 2, 53, 224, 621, 1702, 33, 111, 11005, 11006, 49, 41, 397, 63, 50, 60, 1703, 299, 704, 900, 198, 278, 699, 54, 701, 38, 39, 123, 52, 11015, 1232, 118, 124, 152, 266, 120, 137, 51, 125, 391, 362, 270, 390, 130, 392, 23, 122, 151, 142, 140, 640, 643, 644, 639, 645, 641, 646, 642, 622, 638, 649, 635, 653, 652, 650, 647, 632, 636, 651, 633, 637, 648, 634, 620, 282, 163, 16501, 10091, 8, 10, 21, 16, 7, 7041, 7061, 17, 3, 9, 12, 12021, 18, 13, 14, 20, 5, 629, 507, 525, 508, 569, 585, 570, 526, 541, 501, 527, 528, 529, 586, 587, 571, 597, 517, 572, 502, 543, 573, 574, 503, 547, 516, 580, 530, 531, 518, 532, 558, 524, 539, 545, 575, 588, 509, 548, 589, 559, 549, 560, 561, 584, 538, 590, 542, 510, 540, 546, 550, 511, 576, 551, 562, 512, 552, 544, 563, 519, 533, 534, 591, 596, 577, 535, 592, 536, 578, 593, 520, 553, 594, 595, 579, 537, 564, 146, 700, 28, 30, 135, 117, 37, 800, 999, 11003, 181, 11011, 11008, 11009, 11010, 11012, 11007, 153, 11002, 133, 36, 1619, 35, 1633, 148, 11, 703, 44, 134, 705, 226, 276, 56, 112, 183, 119, 182, 184, 27, 1701, 655, 310, 121, 275, 26, 626, 702, 40, 193, 187, 190, 46, 195, 100, 296]
-doc_types = ["CO","OC","TRA","CH"]
+url = 'http://190.34.178.19/Sicowebconsultas/buscar.aspx'
+#office_ids = ['0035','624','001','145','055','003','016','017']
+office_ids = ["202", "043", "624", "108", "001", "029", "114", "238", "234", "105", "047", "145", "022", "103", "203", "107", "109", "016015", "106", "055", "057", "204", "315", "330", "345", "600", "115", "208", "11018", "274", "360", "110", "212", "375", "521", "522", "311", "11004", "11014", "625", "623", "628", "058", "045", "611", "614", "612", "613", "615", "616", "617", "618", "610", "619", "031", "002", "053", "224", "621", "01702", "033", "111", "11005", "11006", "049", "041", "397", "063", "050", "060", "01703", "299", "704", "900", "198", "278", "699", "054", "701", "038", "039", "123", "052", "11015", "01232", "118", "124", "152", "266", "120", "137", "051", "125", "391", "0362", "270", "390", "130", "392", "023", "122", "151", "142", "140", "640", "643", "644", "639", "645", "641", "646", "642", "622", "638", "649", "635", "653", "652", "650", "647", "632", "636", "651", "633", "637", "648", "634", "620", "282", "0163", "016501", "010091", "008", "010", "021", "016", "007", "007041", "007061", "017", "003", "009", "012", "012021", "018", "013", "014", "020", "005", "629", "507", "525", "508", "569", "585", "570", "526", "541", "501", "527", "528", "529", "586", "587", "571", "597", "517", "572", "502", "543", "573", "574", "503", "547", "516", "580", "530", "531", "518", "532", "558", "524", "539", "545", "575", "588", "509", "548", "589", "559", "549", "560", "561", "584", "538", "590", "542", "510", "540", "546", "550", "511", "576", "551", "562", "512", "552", "544", "563", "519", "533", "534", "591", "596", "577", "535", "592", "536", "578", "593", "520", "553", "594", "595", "579", "537", "564", "146", "700", "028", "030", "135", "117", "037", "800", "999", "11003", "0181", "11011", "11008", "11009", "11010", "11012", "11007", "153", "11002", "133", "036", "01619", "0035", "01633", "148", "011", "703", "044", "134", "705", "226", "276", "056", "112", "0183", "119", "0182", "0184", "027", "01701", "655", "310", "121", "275", "026", "626", "702", "040", "193", "187", "190", "046", "195", "100", "296"]
+#doc_types = ["ACO","CO","OC","TRA","CH","CHEC"]
+years = [2014,2013,2012,2010,2009,2008,2007]
+recent_years = [2014,2013,2012,2010]
+doc_types = ['AC', 'ACO', 'ACUE', 'ACUEM', 'AJOC', 'CASER', 'CAT', 'CCR', 'CCUE', 'CD', 'CDNO', 'CEFA', 'CEFOIN', 'CF', 'CH', 'CHEC', 'CHESB', 'CHI', 'CO', 'COMPP', 'CONV', 'CPA', 'CUE', 'CUEP', 'CUET', 'DEM', 'DEN', 'DJB', 'ENM', 'GC', 'GCOSB', 'LETT', 'LEVE', 'LEVP', 'LEVS', 'LEYTRANSP', 'LIQ', 'MOV', 'OC', 'OF', 'OP', 'PAC', 'PAL', 'PLANAD', 'PLANCOM', 'POD', 'POL', 'QUE', 'RC', 'REC', 'REDPART', 'REEM', 'REEMPRE', 'REISO', 'RES', 'RF', 'RPA', 'SAD', 'SOL', 'SV', 'TRA', 'TRA.CERP', 'TRAS']
+
+db_url = os.environ['panadata_db']
+doc_lock = asyncio.Lock()
+sem = asyncio.Semaphore(7)
+engine = create_engine(db_url, convert_unicode=True, echo=True)
+Base.metadata.create_all(engine)
+session_maker = sessionmaker(bind=engine)
+loop = asyncio.get_event_loop()
+states = set ()
+visited = set()
+
+@asyncio.coroutine
+def get(*args, **kwargs):
+    response = yield from aiohttp.request('GET', *args, **kwargs)
+    return (yield from response.read())
+
+@asyncio.coroutine
+def get_html(url):
+    with (yield from sem):
+         return (yield from get(url))
 
 def crawl_all():
     shuffle(office_ids)
@@ -24,49 +53,37 @@ def crawl_all():
         crawl_oficina(office)
 
 def crawl_oficina(oficina_id):
-    visited = db_worker.get_all_controls()
-    pool = Pool(processes=2)
     logger.info('Crawling %s',oficina_id)
     data = url_generator.parse_har()
-    chunks = ( url_generator.set_doc_type(url_generator.set_oficina_id(data,oficina_id),d).copy() for d in doc_types )
-    pool.imap(crawl,chunks)
-    pool.close()
-    pool.join()
-
-def crawl(data):
-    html_queue = Queue()
-    conn = urllib3.HTTPConnectionPool('190.34.178.19', maxsize=15)
-    for year in [2009,2010,2011,2012,2013,2014]:
-        data = url_generator.set_year(data,year)
-        states = ['_']
-        visited = set()
-        depth = 0
-        while states:
+    for d in doc_types:
+        data = url_generator.set_doc_type(url_generator.set_oficina_id(data,oficina_id),d).copy()
+        for year in recent_years:
+            states.clear()
+            visited.clear()
             docs = set()
-            visited.update(states)
-            queries = states_to_queries(data,states)
-            spawn_threads_from_queries(queries,conn,html_queue)
-            while not html_queue.empty(): docs.add(html_queue.get())
-            visited.update([d.control for d in docs])
-            states = [s for s in docs_to_states(docs) if s not in visited]
-            docs = ( db_worker.create_documento(item) for sublist in docs for item in sublist if item.control not in visited )
-            depth += 1
-        logger.info('visited %i states',len(visited))
-        logger.info('total depth for %i: %i',year,depth)
-    logger.info('found %i docs',len(docs))
-    conn.close()
+            states.add('_')
+            data = url_generator.set_year(data,year)
+            new_tasks = spawn_queries(data,states)
+            while new_tasks:
+                try:
+                    loop.run_until_complete(new_tasks)
+                    new_tasks = spawn_queries(data,states.difference(visited))
+                    visited.update(states)
+                except ValueError:
+                   break
 
-def spawn_threads_from_queries(queries,conn,html_queue,):
-    threads = []
-    while queries:
-        if threading.active_count() - 1 < 10:
-            q = queries.pop()
-            ct = CrawlPagesThread(q,conn,html_queue)
-            ct.setDaemon(True)
-            ct.start()
-            threads.append(ct)
-        sleep(0.1)
-    [t.join() for t in threads]
+def spawn_queries(data,states):
+    queries = states_to_queries(data,states)
+    new_tasks = []
+    for q in queries:
+        new_tasks.append(asyncio.async(crawl_pages(q)))
+    return asyncio.wait(new_tasks)
+
+def process_query(docs,data):
+    session = session_maker()
+    states.update([s for s in docs_to_states(docs) if s not in visited])
+    docs = [ db_worker.create_documento(item,session) for item in docs if item.control not in visited ]
+    session.close()
 
 def states_to_queries(data,states):
    queries = list()
@@ -81,43 +98,40 @@ def state_to_query(data,state):
 
 def docs_to_states(docs):
     states = set()
-    regex = re.compile("\d+")
     for d in docs:
-        if regex.match(d.numero):
-            states.update([item for sublist in [int_to_states(n) for n in regex.findall(d.numero)] for item in sublist])
-        if ' ' in str(d.numero):
-            s = str(d.numero).split(' ')[0][:2] + '__'
-            states.add(s)
-        if '-' in str(d.numero):
-            s = str(d.numero).split('-')
-            states.update(s)
+        tokens = ''.join(c for c in d.numero if c.isalnum() or c.isspace() or c.isdigit() or c == '-').split('-')
+        for token in tokens:
+            if token.isdigit():
+                if token not in [str(y) for y in years]:
+                    states.update(int_to_states(token))
+            else:
+                states.update(token.split())
     return states
 
 def int_to_states(i):
     i = math.floor(int(i)/100)
     return [ str(n) + '__' for n in range(i-2,i+2) ]
 
-class CrawlPagesThread(threading.Thread):
-    def __init__(self,data,conn,html_queue):
-        threading.Thread.__init__(self)
-        self.conn = conn
-        self.data = data
-        self.queue = html_queue
+@asyncio.coroutine
+def post(*args, **kwargs):
+    response = yield from aiohttp.request('POST', *args, **kwargs)
+    if response.status == 200 and 'error' not in response.url:
+        return (yield from response.read())
+    response.close()
 
-    def run(self):
-        for doc in self.crawl_pages(self.data,self.conn):
-            self.queue.put(doc)
-
-    def crawl_pages(self,data,conn):
-        logger.debug('crawling %s',url_generator.get_document_id(data))
-        docs = set()
-        while url_generator.get_page(data) < 5:
-            html = conn.request("POST", url, data).data.decode('ISO-8859-1','ignore')
-            soup = BeautifulSoup(html,'lxml')
-            docs.update(parser.get_documentos(soup))
-            if not parser.has_pages(soup): break
-            data = url_generator.increment_page(data)
-            data = url_generator.set_event_validation(data,parser.get_event_validation(soup))
-            data = url_generator.set_view_state(data,parser.get_view_state(soup))
-        logger.debug('found %i docs',len(docs))
-        return docs
+@asyncio.coroutine
+def crawl_pages(data):
+    logger.info('crawling %s',url_generator.get_document_id(data))
+    docs = set()
+    while url_generator.get_page(data) < 5:
+        with (yield from sem):
+                html = yield from post(url, data=data)
+        soup = BeautifulSoup(html,'lxml')
+        docs.update(parser.get_documentos(soup))
+        if not parser.has_pages(soup): break
+        data = url_generator.increment_page(data)
+        data = url_generator.set_event_validation(data,parser.get_event_validation(soup))
+        data = url_generator.set_view_state(data,parser.get_view_state(soup))
+    with (yield from doc_lock):
+        process_query(docs,data)
+    logger.debug('found %i docs',len(docs))
